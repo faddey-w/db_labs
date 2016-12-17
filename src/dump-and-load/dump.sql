05c7ec026e756d34cefe4f0828acbcc879a2a176
Yaroslav Protsenko <yprotsenko@4tifier.com>
2016-12-17 02:14:13 +0200

    scripts for dumping/loading data

---
+++ src/dump-and-load/dump.sql
@@ -0,0 +1,6 @@
+SELECT * FROM Route INTO OUTFILE 'data/Route.txt';
+SELECT * FROM Train INTO OUTFILE 'data/Train.txt';
+SELECT * FROM Station INTO OUTFILE 'data/Station.txt';
+SELECT * FROM RouteStation INTO OUTFILE 'data/RouteStation.txt';
+SELECT * FROM TrainRun INTO OUTFILE 'data/TrainRun.txt';
+SELECT * FROM Ticket INTO OUTFILE 'data/Ticket.txt';
---
+++ src/dump-and-load/load.sql
@@ -0,0 +1,6 @@
+LOAD DATA INFILE 'Route.txt' INTO TABLE Route;
+LOAD DATA INFILE 'Train.txt' INTO TABLE Train;
+LOAD DATA INFILE 'Station.txt' INTO TABLE Station;
+LOAD DATA INFILE 'RouteStation.txt' INTO TABLE RouteStation;
+LOAD DATA INFILE 'TrainRun.txt' INTO TABLE TrainRun;
+LOAD DATA INFILE 'Ticket.txt' INTO TABLE Ticket;
